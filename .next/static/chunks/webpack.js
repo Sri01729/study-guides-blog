a6c0c80bd37361311e190876c3adad63112701ca
Sri01729 <saialahariedu@gmail.com>
2025-04-29 01:57:54 -0700

    Update .gitignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7af2ea6a70dd3066"; }
+/******/ 		__webpack_require__.h = function() { return "4d08be7a449ff93e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
